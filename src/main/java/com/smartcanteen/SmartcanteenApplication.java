a55b287a8f6a4751486fa5b20ed271728f8cadb3
pushpak <pushpakninave.jee@gmail.com>
2025-07-24 12:26:00 +0000

    did a minimal backend setup

---
+++ src/main/java/com/smartcanteen/SmartcanteenApplication.java
@@ -0,0 +1,13 @@
+package com.smartcanteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcanteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcanteenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartcanteen/SmartcanteenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcanteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcanteenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
