4a668daafd875ffbf944ca6324a16bc84ee58e5d
mdasif22 <imd.asif222@gmail.com>
2025-08-07 23:30:57 +0530

    Test changes

--- src/main/java/com/smartcanteen/controller/FoodController.java
+++ src/main/java/com/smartcanteen/controller/FoodController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+// handles all fooditems
 @RestController
 @RequestMapping("/api/food")
 public class FoodController {
